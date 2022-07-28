be15fbb1fd539ee611d564b7c4ab172000a5afad
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2022-07-28 13:55:00 +0200

    Express setup - server/src/index.ts

---
+++ server/src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express'
+
+const PORT = process.env.PORT ?? 5000
+
+const app = express()
+
+app.use(express.json())
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
