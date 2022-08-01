0d64449c1df927ac390c7a5bea032de84d9eae8d
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2022-08-01 20:31:56 +0200

    Chakra UI setup
    npm i @chakra-ui/react @emotion/react@^11 @emotion/styled@^11 framer-motion@^6
    
    Following this: https://chakra-ui.com/getting-started/cra-guide\#manual-installation

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react'
 import './App.css'
+import { ChakraProvider } from '@chakra-ui/react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { MainContainer } from './MainContainer'
 import { HomePage } from './pages/HomePage'
@@ -10,17 +11,19 @@ import { RecipeDetailPage } from './pages/RecipeDetailPage'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainContainer />}>
-          <Route path="" element={<HomePage />} />
-          <Route path="about" element={<AboutPage />} />
-          <Route path="recipes/new" element={<CreateRecipePage />} />
-          <Route path="recipes/:recipeId" element={<RecipeDetailPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <ChakraProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainContainer />}>
+            <Route path="" element={<HomePage />} />
+            <Route path="about" element={<AboutPage />} />
+            <Route path="recipes/new" element={<CreateRecipePage />} />
+            <Route path="recipes/:recipeId" element={<RecipeDetailPage />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </ChakraProvider>
   )
 }
 
