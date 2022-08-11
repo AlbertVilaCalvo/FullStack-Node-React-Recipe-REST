4578116ba5dbca19bb3a440ddf8bcb231fdd2582
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2022-08-11 19:06:06 +0200

    [server] Add fake delay to responses in development environment

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,14 +1,23 @@
-import express from 'express'
+import express, { RequestHandler } from 'express'
 import { router } from './router'
 
 const PORT = process.env.PORT ?? 5000
 
 const app = express()
 
+const isDevelopment = app.get('env') === 'development'
+
 app.use(express.json())
+
+if (isDevelopment) {
+  const fakeDelay: RequestHandler = (req, res, next) => {
+    setTimeout(next, Math.floor(Math.random() * 800 + 200))
+  }
+  app.use('/api', fakeDelay)
+}
 
 app.use('/api', router)
 
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT} - ${app.get('env')} environment`)
 })
