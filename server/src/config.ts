5c025ab88b06a15e601466ea4b149e82dcd54db3
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2022-08-22 13:31:54 +0200

    Config - add comments with envar name

--- server/src/config.ts
+++ server/src/config.ts
@@ -11,14 +11,21 @@ function getEnvar(environmentVariable: string): string {
 }
 
 type Config = Readonly<{
+  /** PORT */
   port: number
+  /** NODE_ENV */
   env: 'development' | 'production'
   isDevelopment: boolean
 
+  /** DB_NAME */
   databaseName: string
+  /** DB_USER */
   databaseUser: string
+  /** DB_PASSWORD */
   databasePassword: string
+  /** DB_HOST */
   databaseHost: string
+  /** DB_PORT */
   databasePort: number
 }>
 
