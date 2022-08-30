2c4db046c6e6632af4ecdd6b9a23b3fae21e892a
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2022-08-30 16:41:50 +0200

    [server] Change isNumber() arg type to string
    We use it only for strings actually, and it doesn't make sense for numbers

--- server/src/misc/util.ts
+++ server/src/misc/util.ts
@@ -6,12 +6,10 @@ export function requestFullUrl(req: Request): string {
 }
 
 /**
- * Check if the argument is a number or a string that contains a number.
- * Important: if the argument is a string, you need to convert it to a number
- * with `parseFloat()` or `Number()`.
+ * Check if the given string can ve converted to a number with `parseFloat()`
+ * or `Number()`.
  * From https://stackoverflow.com/a/1421988/4034572.
  */
-// eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function isNumber(n: any): boolean {
-  return !isNaN(parseFloat(n)) && !isNaN(n - 0)
+export function isNumber(n: string): boolean {
+  return !isNaN(parseFloat(n))
 }
