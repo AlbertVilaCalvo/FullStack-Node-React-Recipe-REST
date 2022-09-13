a822543018121ea9246d2c00a125a01b13e75bdd
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2022-09-13 18:27:25 +0200

    Test AuthMiddleware.requireLoggedUser

---
+++ server/src/auth/AuthMiddleware.test.ts
@@ -0,0 +1,118 @@
+import * as HttpMocks from 'node-mocks-http'
+import { requireLoggedUser } from './AuthMiddleware'
+import { StatusCode } from '../misc/StatusCode'
+import { generateAuthToken } from './authtoken'
+import { getUserById } from '../user/UserDatabase'
+import { User } from '../user/User'
+
+jest.mock('../user/UserDatabase')
+
+const USER: User = {
+  id: 50,
+  name: 'Pere',
+  password: 'hash',
+  email: 'x@y.co',
+}
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+const next = jest.fn(() => {})
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+function expect401(res: HttpMocks.MockResponse<any>) {
+  expect(res._isEndCalled()).toBe(true)
+  expect(res.statusCode).toBe(StatusCode.UNAUTHORIZED_401)
+  expect(res._isJSON()).toBe(true)
+  expect(res._getJSONData().error.code).toBe('valid_auth_token_required')
+  expect(res._getJSONData().error.message).toContain('Authorization')
+  expect(next.mock.calls.length).toBe(0)
+}
+
+describe('AuthMiddleware.requireLoggedUser', () => {
+  test('should return 401 if the Authorization header is missing', async () => {
+    const req = HttpMocks.createRequest({
+      method: 'POST',
+      url: '/recipes',
+      // missing headers
+    })
+    const res = HttpMocks.createResponse()
+
+    await requireLoggedUser(req, res, next)
+
+    expect401(res)
+  })
+
+  test('should return 401 if the auth token is empty', async () => {
+    const authToken = ''
+    const req = HttpMocks.createRequest({
+      method: 'POST',
+      url: '/recipes',
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    })
+    const res = HttpMocks.createResponse()
+
+    await requireLoggedUser(req, res, next)
+
+    expect401(res)
+  })
+
+  test('should return 401 if the auth token user is not found', async () => {
+    const authToken = await generateAuthToken(USER.id)
+    const req = HttpMocks.createRequest({
+      method: 'POST',
+      url: '/recipes',
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    })
+    const res = HttpMocks.createResponse()
+
+    const getUserByIdMock = jest.mocked(getUserById)
+    getUserByIdMock.mockResolvedValueOnce('user-not-found')
+
+    await requireLoggedUser(req, res, next)
+
+    expect401(res)
+  })
+
+  test('should call the next middleware if the auth token is valid', async () => {
+    const authToken = await generateAuthToken(USER.id)
+    const req = HttpMocks.createRequest({
+      method: 'POST',
+      url: '/recipes',
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    })
+    const res = HttpMocks.createResponse()
+
+    const getUserByIdMock = jest.mocked(getUserById)
+    getUserByIdMock.mockResolvedValueOnce(USER)
+
+    await requireLoggedUser(req, res, next)
+
+    expect(next.mock.calls.length).toBe(1)
+    expect(res._isEndCalled()).toBe(false)
+    expect(res.statusCode).not.toBe(StatusCode.UNAUTHORIZED_401)
+  })
+
+  test('should set req.user if the auth token is valid', async () => {
+    const authToken = await generateAuthToken(USER.id)
+    const req = HttpMocks.createRequest({
+      method: 'POST',
+      url: '/recipes',
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+      },
+    })
+    const res = HttpMocks.createResponse()
+
+    const getUserByIdMock = jest.mocked(getUserById)
+    getUserByIdMock.mockResolvedValueOnce(USER)
+
+    await requireLoggedUser(req, res, next)
+
+    expect(req.user).toEqual(USER)
+  })
+})
