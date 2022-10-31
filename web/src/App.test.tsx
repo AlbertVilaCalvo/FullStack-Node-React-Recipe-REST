3cd5dbfa1b79c12af3bf90fa6edde62fbee59f89
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2022-10-31 17:32:08 +0100

    GitHub actions - run tests on web

--- web/src/App.test.tsx
+++ web/src/App.test.tsx
@@ -4,6 +4,6 @@ import App from './App'
 
 test('renders the footer', () => {
   render(<App />)
-  const footerText = screen.getByText(/The best recipe app/i)
+  const footerText = screen.getByText(/The best recipe manager app/i)
   expect(footerText).toBeInTheDocument()
 })
