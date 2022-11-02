79b7c43a40fe0d9976840f44d2984ff290801822
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2022-11-02 13:09:17 +0100

    GitHub actions - caching of npm dependencies
    Docs: https://github.com/actions/setup-node

--- web/src/ui/layouts/MainLayout.tsx
+++ web/src/ui/layouts/MainLayout.tsx
@@ -62,7 +62,7 @@ export function MainLayout() {
       <footer className="main-container footer">
         <div className="main-container-child-centered">
           <div id="footer-content">
-            <p>The best recipe manager app! 🍒</p>
+            <p>The best recipe manager app! 🥑</p>
           </div>
         </div>
       </footer>
