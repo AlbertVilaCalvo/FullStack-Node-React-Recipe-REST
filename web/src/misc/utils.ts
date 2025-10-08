b1def876b0b4509c97359e0f47bd08151e359ef3
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2025-10-08 13:15:38 +0200

    [web] Fix 'npm run build' tsc errors
    src/App.test.tsx:4:1 - error TS2582: Cannot find name 'test'. Do you need to install type definitions for a test runner? Try `npm i --save-dev @types/jest` or `npm i --save-dev @types/mocha`.
    
    src/App.test.tsx:7:3 - error TS2304: Cannot find name 'expect'.
    
    src/misc/utils.ts:2:31 - error TS2580: Cannot find name 'process'. Do you need to install type definitions for node? Try `npm i --save-dev @types/node`.

--- web/src/misc/utils.ts
+++ web/src/misc/utils.ts
@@ -1,2 +1,1 @@
-// https://create-react-app.dev/docs/adding-custom-environment-variables/
-export const isDev: boolean = process.env.NODE_ENV === 'development'
+export const isDev: boolean = import.meta.env.DEV
