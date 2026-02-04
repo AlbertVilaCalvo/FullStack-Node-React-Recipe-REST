04c3c95218c592b05f0481070fa1fcf3328b0fad
Albert Vila Calvo <vilacalvo.albert@gmail.com>
2026-02-04 12:38:10 +0100

    Fix - At the website deployed to AWS the login form has email 'a@a.com' and password '123456' filled
    This is because import.meta.env.DEV is true, so	NODE_ENV is not 'production', when it should be.
    We'll need to address this in the future, because the development build of React is not suitable for
    production.
    
    Also, the PREFIXES in vite.config.ts was loading all the environment variables, not just the VITE_*.
    
    Note that GitHub Actions doesn't set NODE_ENV.
    Maybe Vite is loading the .env.local from the root dir which has NODE_ENV=development.
    At https://vite.dev/guide/env-and-mode#env-files it says that .env.local is "loaded in all cases,
    ignored by git"

--- web/src/misc/utils.ts
+++ web/src/misc/utils.ts
@@ -1,1 +1,4 @@
-export const isDev: boolean = import.meta.env.DEV
+export const isDev: boolean = import.meta.env.MODE === 'development'
+console.log(`import.meta.env.MODE:`, import.meta.env.MODE)
+console.log(`import.meta.env.DEV:`, import.meta.env.DEV)
+console.log(`import.meta.env.PROD:`, import.meta.env.PROD)
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,7 +3,7 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 const ENV_DIR = './../'
-const PREFIXES = '' // Only load env variables prefixed with VITE_
+const PREFIXES = 'VITE_' // Only load env variables prefixed with VITE_
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
