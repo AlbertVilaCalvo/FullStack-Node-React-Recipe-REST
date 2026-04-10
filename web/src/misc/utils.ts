ea56d48fb90071d96ee26171fee31c7ba8e285b1
Albert Vila Calvo <4823860+AlbertVilaCalvo@users.noreply.github.com>
2026-04-10 16:16:18 +0200

    web.yml - remove unnecessary env VITE_PORT and VITE_API_BASE_URL (#4)
    I guess they are not needed after the work in bc4ad68a6b3badca721d08d2d931141db5b0e863

--- web/src/misc/utils.ts
+++ web/src/misc/utils.ts
@@ -2,3 +2,8 @@ export const isDev: boolean = import.meta.env.MODE === 'development'
 console.log(`import.meta.env.MODE:`, import.meta.env.MODE)
 console.log(`import.meta.env.DEV:`, import.meta.env.DEV)
 console.log(`import.meta.env.PROD:`, import.meta.env.PROD)
+console.log(`import.meta.env.VITE_PORT:`, import.meta.env.VITE_PORT)
+console.log(
+  `import.meta.env.VITE_API_BASE_URL:`,
+  import.meta.env.VITE_API_BASE_URL
+)
